9725a770c18808e37abe15abc44e4839c987ec73
Emyrreis <vitoria.emylly0405@gmail.com>
2025-09-20 09:50:36 -0300

    implementação do pix + alteração de senha e limite

--- bank.py
+++ bank.py
@@ -36,19 +36,63 @@ class Account:
     def check_password(self, pwd):
         return self.password == pwd
     
+    def change_password(self, old_pwd, new_pwd):
+        if self.check_password(old_pwd):
+            self.password = new_pwd
+            return True
+        return False
+    
     def deposit(self, value: float):
         if value > 0:
             self.__balance += value
-            self.__transactions.append(f"Depósito: +R$ {value:.2f}")
+            self.__transactions.append(f"{datetime.now()} | Depósito: +R${value:.2f}")
             return True
         return False
     
     def withdraw(self, value: float):
         if value > 0 and (self.__balance - value) >= self.__limit:
             self.__balance -= value
-            self.__transactions.append(f"Saque: -R$ {value:.2f}")
+            self.__transactions.append(f"{datetime.now()} | Saque: -R${value:.2f}")
             return True
         return False
 
     def extrato(self):
         return self.__transactions
+    
+    #Métodos do pix
+    def create_pix_key(self, key: str):
+        if not self.pix_key:
+            self.pix_key = key
+            return True
+        return False
+    
+    def send_pix(self, value: float, destination_key: str, conta1, conta2):
+        if value <= 0:
+            return False
+
+        if (self.balance - value) < self.limit:
+            return False
+
+        destination_account = None
+        for c in (conta1, conta2):
+            if str(c.pix_key) == str(destination_key):
+                destination_account = c
+                break
+
+        if not destination_account:
+            return False
+
+        self._Account__balance -= value
+        destination_account._Account__balance += value
+
+        # Registra no extrato junto com as movimentações do saque e depósito
+        self._Account__transactions.append(f"{datetime.now()} | Pix enviado: -R${value:.2f} para {destination_account.name}")
+        destination_account._Account__transactions.append(f"{datetime.now()} | Pix recebido: +R${value:.2f} de {self.name}")
+
+        return True
+
+    #Método do limite
+    def change_limit(self, new_limit: float):
+        self.__limit = new_limit
+        self.__transactions.append(f"{datetime.now()} | Alteração de limite: R${new_limit:.2f}")
+        return True
--- main.py
+++ main.py
@@ -1,48 +1,91 @@
 from bank import Account
 
 #Testando
-conta = Account("Emilly Reis", "0001", "12345-6", "1234", balance = 1000.0)
+conta1 = Account("Emilly Reis", "0001", "12345-6", "1234", balance = 1000.0)
+conta2 = Account("Bella Santos", "0002", "67891-1", "4321", balance = 500.0)
+
+conta2.create_pix_key(123456789)
 
 while True:
-    print("APP Bank")
+    print("\nAPP Bank")
     senha = input("Digite sua senha: ")
-    if conta.check_password(senha):
+    if conta1.check_password(senha):
+        print(f"\nNome: {conta1.name}")
+        print(f"Conta: {conta1.account_number}")
+        print(f"Agência: {conta1.agency}")
+        print(f"Saldo: {conta1.balance}")
+
         while True:
-            print(f"Bem Vindo(a) {conta.name}")
+            print(f"\nBem Vindo(a) {conta1.name}")
             print("1 - Depositar")
             print("2 - Sacar")
             print("3 - Ver extrato")
-            print("4 - Sair")
+            print("4 - Criar chave Pix")
+            print("5 - Enviar Pix")
+            print("6 - Alterar limite")
+            print("7 - Alterar senha")
+            print("8 - Sair")
 
-            opcao = input("Escolha uma opção: ")
+            opcao = input("\nEscolha uma opção: ")
 
             if opcao == "1":
                 valor = float(input("Digite o valor do depósito: "))
-                if conta.deposit(valor):
+                if conta1.deposit(valor):
                     print("Depósito realizado com sucesso!")
+                    print(f"Saldo atual: R${conta1.balance:.2f}")
                 else:
                     print("Valor inválido.")
-            
+
             elif opcao == "2":
                 valor = float(input("Digite o valor do saque: "))
-                if conta.withdraw(valor):
+                if conta1.withdraw(valor):
                     print("Saque realizado com sucesso!")
+                    print(f"Saldo atual: R${conta1.balance:.2f}")
                 else:
-                    print("Saque não permitido (saldo insuficiente).")
-            
+                    print("Saque não permitido (valor inválido ou saldo insuficiente).")
+
             elif opcao == "3":
-                print("Extrato:")
-                for t in conta.extrato():
+                print("Extrato: ")
+                for t in conta1.extrato():
                     print("-", t)
-                print(f"Saldo atual: R$ {conta.balance:.2f}")
-            
+                print(f"\n- Saldo atual: R${conta1.balance:.2f}")
+
             elif opcao == "4":
+                chave = input("Digite a chave Pix para cadastrar: ")
+                if conta1.create_pix_key(chave):
+                    print("Chave Pix cadastrada com sucesso!")
+                else:
+                    print(f"Você já possui uma chave Pix cadastrada: {conta1.pix_key}")
+
+            elif opcao == "5":
+                valor = float(input("Digite o valor do Pix: "))
+                keyp = input("Digite a chave Pix: ")
+                if conta1.send_pix(valor, keyp, conta1, conta2):
+                    print(f"Pix de R${valor:.2f} enviado com sucesso!")
+                else:
+                    print("Erro ao enviar Pix (verifique saldo ou chave do destino).")
+
+            elif opcao == "6":
+                print(f"Limite atual: R$ {conta1.limit:.2f}")
+                novo_limite = float(input("Digite o novo limite: "))
+                conta1.change_limit(novo_limite)
+                print(f"Limite alterado para R${novo_limite:.2f} com sucesso!")
+
+            elif opcao == "7":
+                antiga = input("Digite sua senha atual: ")
+                nova = input("Digite a nova senha: ")
+                if conta1.change_password(antiga, nova):
+                    print("Senha alterada com sucesso. Faça login novamente.")
+                    break 
+                else:
+                    print("Senha atual incorreta.")
+
+            elif opcao == "8":
                 print("Saindo...")
                 break
 
             else:
                 print("Opção inválida.")
+
     else:
-        print("Senha inválida")
-
-
+        print("Senha inválida! Tente novamente.\n")
